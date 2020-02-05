c977815a5b6e632ad5eb290795a12fb0ec7ef505
Dan Foley <dan@dan-foley.com>
2020-02-05 19:27:03 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,8 +4,24 @@ class Doctor
 
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new()
+    
   end
   
 end
